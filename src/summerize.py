00f53c8f1d898815f5e784d1df687b1fe6df0580
Mohamed Elkhiat <mohammed.khayyat02@eng-st.cu.edu.eg>
2024-07-18 10:38:47 +0300

    Clustered the text

--- src/summerize.py
+++ src/summerize.py
@@ -1,8 +1,28 @@
-import Doc
+import Doc, Embedding as emb
+import numpy as np
 
+from sklearn.cluster import KMeans
+from dotenv import load_dotenv
+
+def get_indices(num_clusters):
+    closest_indices = []
+
+    for i in range(num_clusters):
+        distances = np.linalg.norm(vectors - kmeans.cluster_centers_[i], axis=1)
+        closest_index = np.argmin(distances)
+        closest_indices.append(closest_index)
+        selected_indices = sorted(closest_indices)
+    
+    return selected_indices
+
+load_dotenv()
 doc = Doc.Document()
 doc.load_from_pdf('./Books/Cover Letter.pdf')
+
 cleaned_text = doc.data
+docs, vectors = emb.generate_embedding(open_source=False, text=cleaned_text)
 
-print(cleaned_text[:10])
+num_clusters = 5
+kmeans = KMeans(n_clusters=num_clusters, random_state=42).fit(vectors)
 
+selected_indices = get_indices(num_clusters)
