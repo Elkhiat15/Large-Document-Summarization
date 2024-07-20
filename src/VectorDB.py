8987190139332583761b5502521e381b443076c0
3luka <94873742+MohamedAlaaAli@users.noreply.github.com>
2024-07-20 10:04:48 +0300

    cohere docstrings in VectorDB.py

--- src/VectorDB.py
+++ src/VectorDB.py
@@ -9,21 +9,21 @@ class VectorDataBase:
 
     def __init__(self, use_open_source=False):
         """
-        Initialize VectorDataBase instance.
+            Initialize VectorDataBase instance.
 
-        This function initializes a VectorDataBase instance with the specified parameters.
-        It loads environment variables using dotenv, sets the dataset path, and initializes
-        the vector database (DeepLake) with the appropriate embedding model based on the
-        'use_open_source' flag.
+            This function initializes a VectorDataBase instance with the specified parameters.
+            It loads environment variables using dotenv, sets the dataset path, and initializes
+            the vector database (DeepLake) with the appropriate embedding model based on the
+            'use_open_source' flag.
 
-        Parameters:
-        use_open_source (bool): A flag indicating whether to use an open-source embedding model.
-                                If True, HuggingFaceEmbeddings will be used; otherwise,
-                                GoogleGenerativeAIEmbeddings will be used.
-                                Default is False.
+                Parameters:
+                    use_open_source (bool): A flag indicating whether to use an open-source embedding model.
+                                            If True, HuggingFaceEmbeddings will be used; otherwise,
+                                            GoogleGenerativeAIEmbeddings will be used.
+                                            Default is False.
 
-        Returns:
-        None
+                Returns:
+                    None
         """
         load_dotenv()
         self.use_open_source = use_open_source
@@ -33,17 +33,17 @@ class VectorDataBase:
     
     def add_to_database(self,text):
         """
-        Add documents to the vector database.
+            Add documents to the vector database.
 
-        This function takes a text input, splits it into smaller chunks based on the specified parameters,
-        and adds these chunks as documents to the vector database. The chunk size and overlap are determined
-        based on the 'use_open_source' flag.
+            This function takes a text input, splits it into smaller chunks based on the specified parameters,
+            and adds these chunks as documents to the vector database. The chunk size and overlap are determined
+            based on the 'use_open_source' flag.
 
-        Parameters:
-        text (str): The input text to be added to the vector database.
+                Parameters:
+                    text (str): The input text to be added to the vector database.
 
-        Returns:
-        None
+                Returns:
+                    None
         """
         docs = chunck(text, 
                       size=512 if self.use_open_source else 6000, 
